5682ba6415aa45afffc0e93b4bfe5305fc47a2dd
piva1177 <dani.alexa17@gmail.com>
2023-08-17 10:58:29 -0500

    17/08/2023 10:58

--- funciones.js
+++ funciones.js
@@ -49,28 +49,49 @@ function cargarMunicipios2() {
         municipioSelect.appendChild(option);
     });
 }
- // Esta función se encarga de verificar si los correos electrónicos coinciden
-function verificarCorreos() {
-    const correo1 = document.getElementById("correo1").value;
-    const correo2 = document.getElementById("correo2").value;
-    
-    if (correo1 !== correo2) {
-        document.getElementById("correo2").setCustomValidity("Los correos electrónicos no coinciden.");
-    } else {
-        document.getElementById("correo2").setCustomValidity("");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botonEnviar = document.querySelector(".boton-enviar");
+    const modalConfirmacion = new bootstrap.Modal(document.getElementById("confirmacionModal"));
+    const miFormulario = document.getElementById("miFormulario");
+
+    botonEnviar.addEventListener("click", function (event) {
+        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+
+        // Verificar si el formulario está válido antes de mostrar el modal
+        if (miFormulario.checkValidity()) {
+            modalConfirmacion.show(); // Abrir modal de confirmación si el formulario es válido
+        } else {
+            // Si el formulario no es válido, se muestra una alerta
+            alert("Debes llenar todos los campos requeridos antes de enviar el formulario.");
+        }
+    });
+
+    const confirmarEnvio = document.getElementById("confirmarEnvio");
+    confirmarEnvio.addEventListener("click", function () {
+        modalConfirmacion.hide(); // Cierra el modal de confirmación
+        miFormulario.submit(); // Envía el formulario
+    });
+
+    function verificarCorreos() {
+        const correo1 = document.getElementById("correo1").value;
+        const correo2 = document.getElementById("correo2").value;
+
+        if (correo1 !== "" && correo2 !== "") {
+            if (correo1 === correo2) {
+                document.getElementById("correo2").setCustomValidity(""); // Correos coinciden, elimina el mensaje de error
+            } else {
+                document.getElementById("correo2").setCustomValidity("Los correos electrónicos no coinciden."); // Correos no coinciden, establece el mensaje de error
+            }
+        } else {
+            document.getElementById("correo2").setCustomValidity(""); // Uno de los campos está vacío, no hay mensaje de error
+        }
+
+        // Actualiza la validación del formulario
+        miFormulario.reportValidity();
     }
-}
 
-// Agregar el evento input a los campos de correo electrónico
-document.getElementById("correo1").addEventListener("input", verificarCorreos);
-document.getElementById("correo2").addEventListener("input", verificarCorreos);
-
-document.getElementById("miFormulario").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
-    
-    // Aquí puedes agregar código para mostrar una alerta de éxito
-    alert("El formulario se envió correctamente.");
-    
-    // Limpia el formulario si es necesario
-    this.reset();
+    document.getElementById("correo1").addEventListener("input", verificarCorreos);
+    document.getElementById("correo2").addEventListener("input", verificarCorreos);
 });
